36c3515e3b2852b690f8f922bf0c18d999566667
NiltonSV <incripcion18@gmail.com>
2025-09-10 10:02:33 -0300

    add utility package

---
+++ arms_utils/arms_utils/replace_string.py
@@ -0,0 +1,37 @@
+import os
+import re
+
+
+def replace_string(file_path, old_strings, new_strings):
+    """Read a template file, perform replacements, and save it with the same extension."""
+    with open(file_path, 'r') as srdf_file:
+        srdf_content = srdf_file.read()
+
+    ext = re.sub(r'.*\.', '', os.path.basename(file_path))
+    base = re.sub(r'_template\.[^.]+$', '', os.path.basename(file_path))
+
+    for old_string, new_string in zip(old_strings, new_strings):
+        srdf_content = srdf_content.replace(old_string, new_string)
+
+    new_path = os.path.join(os.path.dirname(file_path), f'{base}.{ext}')
+
+    with open(new_path, 'w') as f:
+        f.write(srdf_content)
+
+    # print(f'New path: {new_path}')
+
+    return new_path
+
+
+def main():
+    from ament_index_python.packages import get_package_share_directory
+
+    test_file = os.path.join(get_package_share_directory('gen3_lite_moveit_config'),
+                             'config', 'gen3_lite_kinova_2f_lite', 'gen3_lite_template.srdf')
+
+    print(f'Test path: {test_file}')
+    print(f'New path: {replace_string(test_file, '<robot_prefix>', 'kinova_')}')
+
+
+if __name__ == "__main__":
+    main()
